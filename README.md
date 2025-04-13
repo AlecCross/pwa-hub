fb671203b6d20c1d61ce27874cf5214972cab135
AlecCross <cross2472@gmail.com>
2025-04-13 20:50:11 +0000

    Use Serwist

--- README.md
+++ README.md
@@ -1,40 +1,40 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/pages/api-reference/create-next-app).
+# Serwist example - next-basic
 
-## Getting Started
+This example demonstrates how to use Serwist with Next.js.
 
-First, run the development server:
+## Usage
+
+[![Open in Gitpod and run](https://img.shields.io/badge/Open%20In-Gitpod.io-%231966D2?style=for-the-badge&logo=gitpod)](https://gitpod.io/#https://github.com/serwist/serwist/)
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+cd examples/next-basic
+pnpm build
+pnpm start
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+or
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+Execute [`degit`](https://github.com/Rich-Harris/degit) with [npm](https://docs.npmjs.com/cli/init), [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/), [pnpm](https://pnpm.io), or [bun](https://bun.sh) to bootstrap the example:
 
-[API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+```bash
+npx degit serwist/serwist/examples/next-basic my-app
+```
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/pages/building-your-application/routing/api-routes) instead of React pages.
+```bash
+yarn degit serwist/serwist/examples/next-basic my-app
+```
 
-This project uses [`next/font`](https://nextjs.org/docs/pages/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+```bash
+pnpx degit serwist/serwist/examples/next-basic my-app
+```
 
-## Learn More
+```bash
+bunx degit serwist/serwist/examples/next-basic my-app
+```
 
-To learn more about Next.js, take a look at the following resources:
+## Recommended `.gitignore`
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn-pages-router) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/pages/building-your-application/deploying) for more details.
+```gitignore
+public/sw*
+public/swe-worker*
+```
--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import styled from 'styled-components';
 import AppCard from '../components/AppCard';
 
@@ -24,55 +26,67 @@ const apps = [
     name: 'Розкладка',
     description: 'Перетворювач розкладки клавіатури',
     link: 'https://rozkladka.vercel.app/',
-    icon: '/apps-icons/Rozkladka/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/Rozkladka/icon-512.webp', 
   },
   {
     name: 'Img2Text Liard',
     description: 'Розпізнавання тексту з зображень',
     link: 'https://img2text-liard.vercel.app/',
-    icon: '/apps-icons/img2text/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/img2text/icon-512.webp', 
   },
   {
     name: 'Next PWA Icons',
     description: 'Генератор іконок для PWA',
     link: 'https://next-pwa-icons.vercel.app/',
-    icon: '/apps-icons/icons for PWA Webp/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/icons for PWA Webp/icon-512.webp', 
   },
   {
     name: 'TxtDiff',
     description: 'Порівняння текстових файлів',
     link: 'https://txtdiff.vercel.app/',
-    icon: '/apps-icons/txtdiff-icons/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/txtdiff-icons/icon-512.webp', 
   },
   {
     name: 'PWA Player',
     description: 'Аудіо- та відеопрогравач',
     link: 'https://pwa-player.vercel.app/',
-    icon: '/apps-icons/pwa-player/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/pwa-player/icon-512.webp', 
   },
   {
     name: 'Next Stopwatch',
     description: 'Секундомір та таймер',
     link: 'https://next-stopwatch.vercel.app/',
-    icon: '/apps-icons/stopwatch/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/stopwatch/icon-512.webp', 
   },
   {
     name: 'Next Audio Extractor',
     description: 'Витягнення аудіо з відео',
     link: 'https://next-audio-extractor.vercel.app/',
-    icon: '/apps-icons/AudioExtractorPWA-icons/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/AudioExtractorPWA-icons/icon-512.webp', 
   },
   {
     name: 'Amoled Saver',
     description: 'Збереження заряду AMOLED-екрану',
     link: 'https://amoled-saver.vercel.app/',
-    icon: '/apps-icons/Black Screensaver PWA icons/icon-512.webp', // Додайте шлях до вашого значка
+    icon: '/apps-icons/Black Screensaver PWA icons/icon-512.webp', 
   },
   {
     name: 'Next FFmpeg',
     description: 'Інтерфейс для FFmpeg',
-    link: 'https://vercel.com/aleccross-projects/next-ffmpeg',
-    icon: '/apps-icons/ffmpeg/icon-512x512.png', // Додайте шлях до вашого значка
+    link: 'https://next-ffmpeg.vercel.app',
+    icon: '/apps-icons/ffmpeg/icon-512x512.png', 
+  },
+  {
+    name: 'RGB Wall',
+    description: 'Create Walpaper',
+    link: 'https://rgb-wall.vercel.app',
+    icon: '/apps-icons/rgb-wall/icon-512.webp', 
+  },
+  {
+    name: 'Gumi-sounds',
+    description: 'funny notification',
+    link: 'https://gumi-sounds.vercel.app',
+    icon: '/apps-icons/Gumi-sounds/icon-512-512.webp',
   },
 ];
 
